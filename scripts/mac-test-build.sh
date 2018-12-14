5043f320240768359e9408b064743ca2b8cc0dff
PhucNgo1711 <phucnmrot@gmail.com>
2018-12-14 16:13:16 -0500

    Second time fixing Travis CI crash osx

--- scripts/mac-test-build.sh
+++ scripts/mac-test-build.sh
@@ -2,11 +2,8 @@
 
 #!/bin/bash
 
-# CaPTk Packager
-
-# Cmake command to run from /trunk/bin
-# We need this directory structure for appimages to be generated
-RUN_CMD () {
+# Set up environment variables
+ENV_CMD() {
 export PATH="$PATH:/usr/local/Cellar/qt/5.11.2/lib/cmake/Qt5:/usr/local/Cellar/qt/5.11.2/bin"
 
 export CMAKE_PREFIX_PATH=/Users/PhucNgo/Desktop/CaPTk/dependency_manager/bin/ITK-build:/Library/TeX/texbin
@@ -15,42 +12,76 @@ export CC=/usr/local/opt/llvm/bin/clang
 export CXX=/usr/local/opt/llvm/bin/clang++
 export LDFLAGS="-L/usr/local/opt/llvm/lib"
 export CPPFLAGS="-L/usr/local/opt/llvm/include"
+}
 
+# CaPTk Packager
+# Cmake command to run from /trunk/bin
+# We need this directory structure for appimages to be generated
+CAPTK_CMD () {
 # cmake -DBUILD_DOCUMENTATION=OFF ..
 cmake ../
 
 cmake ../
 }
 
-echo "[!] Warn: This script is intended for CI use. Only use it if you know what you are doing."
+# Dependency manager
+DEP_MNGR_CMD() {
+cd ../
 
-echo "[:] Starting CaPTk packaging process..."
+git clone https://github.com/PhucNgo1711/CaPTk.git dependency_manager
 
-echo "[?] Checking if you are in trunk..."
-# Test to see if the user is in trunk
-# First see if CMakeLists.txt exists
-if [[ -e CMakeLists.txt ]] ; then
-# See if it contains PROJECT( CaPTk )
-if [[ -z `cat CMakeLists.txt | grep "PROJECT( CaPTk )"` ]] ; then
-echo "[!] Error: You do not appear to be within trunk of CaPTk (Project is not CaPTk)"
-exit -1
-fi
-else
-echo "[!] Error: You do not appear to be in trunk (CMakeLists.txt not found)"
-exit -1
-fi
+cd CaPTk
+git checkout dependency_manager
 
-# Create binary directory
-echo "[:] Creating binary directory..."
 mkdir bin
 cd bin
 
-# Cmake
-echo "[:] Running cmake command..."
-RUN_CMD
+cmake ../
+make 
 
-# Make install/strip
-echo "[:] Building CaPTk..."
-make
+cd ../../CaPTk
+}
 
-echo "[:] Done. Built test target"
+###########################
+echo "[!] Warn: This script is intended for CI use. Only use it if you know what you are doing."
+
+pwd
+cd /usr/
+ls
+
+# echo "[:] Starting CaPTk packaging process..."
+
+# echo "[?] Checking if you are in trunk..."
+# # Test to see if the user is in trunk
+# # First see if CMakeLists.txt exists
+# if [[ -e CMakeLists.txt ]] ; then
+# # See if it contains PROJECT( CaPTk )
+# if [[ -z `cat CMakeLists.txt | grep "PROJECT( CaPTk )"` ]] ; then
+# echo "[!] Error: You do not appear to be within trunk of CaPTk (Project is not CaPTk)"
+# exit -1
+# fi
+# else
+# echo "[!] Error: You do not appear to be in trunk (CMakeLists.txt not found)"
+# exit -1
+# fi
+
+# echo "[:] Set up env..."
+# ENV_CMD
+
+# echo "[:] Building dependency manager..."
+# DEP_MNGR_CMD
+
+# # Create binary directory
+# echo "[:] Creating binary directory..."
+# mkdir bin
+# cd bin
+
+# # Cmake
+# echo "[:] Running cmake command..."
+# CAPTK_CMD
+
+# # Make install/strip
+# echo "[:] Building CaPTk..."
+# make
+
+# echo "[:] Done. Built test target"
