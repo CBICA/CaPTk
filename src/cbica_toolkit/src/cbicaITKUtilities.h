e89b4dfed0e306cd5770900c04bf5ffd029988bc
surrsurus <surrsurus@protonmail.com>
2019-01-09 09:12:11 -0500

    Fix smart pointer initialization on gcc/linux

--- src/cbica_toolkit/src/cbicaITKUtilities.h
+++ src/cbica_toolkit/src/cbicaITKUtilities.h
@@ -1001,17 +1001,17 @@ namespace cbica
     resampler->SetTransform(itk::IdentityTransform< double, TImageType::ImageDimension >::New());
     if (interpolator_wrap == "bspline")
     {
-      auto interpolatorFunc = typename itk::BSplineInterpolateImageFunction< TImageType, double >::New();
+      auto interpolatorFunc = itk::BSplineInterpolateImageFunction< TImageType, double >::New();
       resampler->SetInterpolator(interpolatorFunc);
     }
     else if (interpolator_wrap.find("nearest") != std::string::npos)
     {
-      auto interpolatorFunc = typename itk::NearestNeighborInterpolateImageFunction< TImageType, double >::New();
+      auto interpolatorFunc = itk::NearestNeighborInterpolateImageFunction< TImageType, double >::New();
       resampler->SetInterpolator(interpolatorFunc);
     }
     else
     {
-      auto interpolatorFunc = typename itk::LinearInterpolateImageFunction< TImageType, double >::New();
+      auto interpolatorFunc = itk::LinearInterpolateImageFunction< TImageType, double >::New();
       resampler->SetInterpolator(interpolatorFunc);
     }
     resampler->UpdateLargestPossibleRegion();
@@ -1057,12 +1057,12 @@ namespace cbica
     resampler->SetTransform(itk::IdentityTransform< double, TImageType::ImageDimension >::New());
     if (interpolator_wrap == "bspline")
     {
-      auto interpolatorFunc = typename itk::BSplineInterpolateImageFunction< TImageType, double >::New();
+      auto interpolatorFunc = itk::BSplineInterpolateImageFunction< TImageType, double >::New();
       resampler->SetInterpolator(interpolatorFunc);
     }
     else 
     {
-      auto interpolatorFunc = typename itk::LinearInterpolateImageFunction< TImageType, double >::New();
+      auto interpolatorFunc = itk::LinearInterpolateImageFunction< TImageType, double >::New();
       resampler->SetInterpolator(interpolatorFunc);
     }
     resampler->UpdateLargestPossibleRegion();
