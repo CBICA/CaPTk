ec755642767852dded468696e2d7c068502cc2aa
PhucNgo1711 <phucnmrot@gmail.com>
2019-01-12 08:32:55 -0500

    Fixing Travis CI

--- cmake_modules/External-VTK.cmake
+++ cmake_modules/External-VTK.cmake
@@ -24,21 +24,21 @@ ExternalProject_Add(
     -DBUILD_EXAMPLES:BOOL=OFF # examples are not needed
     -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS} # no static builds
     -DBUILD_TESTING:BOOL=OFF 
-    #-DVTK_Group_Qt:BOOL=ON # [QT] dependency, enables better GUI
+    -DVTK_Group_Qt:BOOL=ON # [QT] dependency, enables better GUI
     -DVTK_LEGACY_SILENT:BOOL=ON # disables the QVTKWidget warning
     -DCMAKE_CXX_MP_FLAG=ON 
     -DVTK_Group_Imaging=ON 
-    #-DModule_vtkGUISupportQt:BOOL=ON 
-    #-DModule_vtkGUISupportQtOpenGL:BOOL=ON 
-    #-DModule_vtkRenderingQt:BOOL=ON 
-    #-DModule_vtkViewsQt:BOOL=ON
-    #-DVTK_USE_QTCHARTS:BOOL=ON
-    #-DVTK_USE_QVTK_QTOPENGL:BOOL=ON    
+    -DModule_vtkGUISupportQt:BOOL=ON 
+    -DModule_vtkGUISupportQtOpenGL:BOOL=ON 
+    -DModule_vtkRenderingQt:BOOL=ON 
+    -DModule_vtkViewsQt:BOOL=ON
+    -DVTK_USE_QTCHARTS:BOOL=ON
+    -DVTK_USE_QVTK_QTOPENGL:BOOL=ON    
     -DCMAKE_DEBUG_POSTFIX:STRING=d
-    #-DVTK_QT_VERSION:STRING=5
+    -DVTK_QT_VERSION:STRING=5
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_BINARY_DIR}/install
-    #-DQt5_DIR=${CAPTK_QT5_DIR}
+    -DQt5_DIR=${CAPTK_QT5_DIR}
 )
 
 SET( VTK_DIR ${CMAKE_BINARY_DIR}/VTK-build )
