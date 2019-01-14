0dcf66df6a2fa3c0d8e07050a3ae1a32cb818b40
PhucNgo1711 <phucnmrot@gmail.com>
2019-01-14 09:06:41 -0500

    Fixing Travis CI

--- cmake_modules/Superbuild.cmake
+++ cmake_modules/Superbuild.cmake
@@ -100,14 +100,14 @@ INCLUDE( ExternalProject )
 #MESSAGE( STATUS "Adding YAML-CPP 0.6.2 ...")
 #INCLUDE( ${PROJECT_SOURCE_DIR}/cmake_modules/External-yaml-cpp.cmake )
 
-# MESSAGE( STATUS "Adding EIGEN-3.3.4 ...")
-# INCLUDE( ${PROJECT_SOURCE_DIR}/cmake_modules/External-Eigen.cmake )
+MESSAGE( STATUS "Adding EIGEN-3.3.4 ...")
+INCLUDE( ${PROJECT_SOURCE_DIR}/cmake_modules/External-Eigen.cmake )
 
-# MESSAGE( STATUS "Adding OpenCV-3.4.1 ...")
-# INCLUDE( ${PROJECT_SOURCE_DIR}/cmake_modules/External-OpenCV.cmake )
+MESSAGE( STATUS "Adding OpenCV-3.4.1 ...")
+INCLUDE( ${PROJECT_SOURCE_DIR}/cmake_modules/External-OpenCV.cmake )
 
 MESSAGE( STATUS "Adding VTK-8.1.0 ...")
 INCLUDE( ${PROJECT_SOURCE_DIR}/cmake_modules/External-VTK.cmake )
 
-#MESSAGE( STATUS "Adding ITK-4.13.0 ...")
-#INCLUDE( ${PROJECT_SOURCE_DIR}/cmake_modules/External-ITK.cmake )
+MESSAGE( STATUS "Adding ITK-4.13.0 ...")
+INCLUDE( ${PROJECT_SOURCE_DIR}/cmake_modules/External-ITK.cmake )
