4e849e57ddfb5cab1d6628c15f126dc3f3dd1031
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-01-20 17:08:56 -0500

    fixed issues brought on by previous PR

--- src/applications/PseudoProgressionEstimator.h
+++ src/applications/PseudoProgressionEstimator.h
@@ -30,8 +30,8 @@ See COPYING file or https://www.med.upenn.edu/sbia/software-agreement.html
 #include "itkScalarImageToRunLengthFeaturesFilter.h"
 #include "itkCSVArray2DFileReader.h"
 #include "itkConnectedComponentImageFilter.h"
-#include "CapTkEnums.h"
-#include "CapTkClassifierUtils.h"
+#include "CaPTkEnums.h"
+#include "CaPTkClassifierUtils.h"
 #include "cbicaLogging.h"
 
 #define RECURRENCE_MODEL_G 0.5
