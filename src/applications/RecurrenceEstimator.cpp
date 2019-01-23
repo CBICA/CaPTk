7d1aaaf42c85dc11d31266f1fedce1a3e5a52fc6
saima rathore <saima.rathore@uphs.upenn.edu>
2019-01-23 10:56:08 -0500

    corrections for the bug-fixed release in survival predictor, perfusion and diffusion derivatives

--- src/applications/RecurrenceEstimator.cpp
+++ src/applications/RecurrenceEstimator.cpp
@@ -629,15 +629,16 @@ bool RecurrenceEstimator::RecurrenceEstimateOnExistingModel(std::vector<std::map
 		for (unsigned int index = 0; index < result_modified.size(); index++)
 			RecProbabilityMap->SetPixel(testindices[index], result_revised[index] * 1);
 
-		//averaging filter
-		typedef itk::MeanImageFilter<ImageType, ImageType > FilterType;
-		FilterType::Pointer meanFilter = FilterType::New();
-		FilterType::InputSizeType radius;
-		radius.Fill(1);
-		meanFilter->SetRadius(radius);
-		meanFilter->SetInput(RecProbabilityMap);
-		ImageType::Pointer RevisedRecurrenceMap = meanFilter->GetOutput();
+		////averaging filter
+		//typedef itk::MeanImageFilter<ImageType, ImageType > FilterType;
+		//FilterType::Pointer meanFilter = FilterType::New();
+		//FilterType::InputSizeType radius;
+		//radius.Fill(1);
+		//meanFilter->SetRadius(radius);
+		//meanFilter->SetInput(RecProbabilityMap);
+		//ImageType::Pointer RevisedRecurrenceMap = meanFilter->GetOutput();
 	
+    ImageType::Pointer RevisedRecurrenceMap = RecurrenceMapPostprocessingForBackground<ImageType>(RecProbabilityMap, dilatedEdema);
 		if (imagetype == CAPTK::ImageExtension::NIfTI)
 			mOutputLocalPtr.WriteRecurrenceOutputInNifti<ImageType>(RevisedRecurrenceMap, outputdirectory + "/" + static_cast<std::string>(currentsubject[CAPTK::ImageModalityType::IMAGE_TYPE_SUDOID]));
 		else
