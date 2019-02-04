04c3d8871e8c00e9bc229ea8fe04440b04ff41d1
surrsurus <surrsurus@protonmail.com>
2019-02-04 09:19:37 -0500

    Test

--- .travis/linux-generic.sh
+++ .travis/linux-generic.sh
@@ -14,6 +14,7 @@ CMAKE_CMD () {
 
 echo "[!] Warn: This script is intended for CI use. Only use it if you know what you are doing."
 
+
 echo "[:] Starting CaPTk packaging process..."
 
 echo "[?] Checking if you are in trunk..."
