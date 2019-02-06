bb7184920fd744152732456c086eb897ce16f4c8
PhucNgo1711 <phucnmrot@gmail.com>
2019-02-06 13:46:48 -0500

    Fixing macOS

--- .travis/mac-generic.sh
+++ .travis/mac-generic.sh
@@ -17,8 +17,8 @@ cp -r qt /usr/local/Cellar/qt
 brew link --force qt
 
 echo "Run Dependency Manager"
-make & sleep 5600; kill $! 
-# make
+# make & sleep 5600; kill $! 
+make
 
 # export CC=/usr/local/opt/llvm/bin/clang
 # export CXX=/usr/local/opt/llvm/bin/clang++
