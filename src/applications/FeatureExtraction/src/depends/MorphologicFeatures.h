c43af0a47247fa69de2a13fe913a4f985c8c194a
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-03-01 11:08:10 -0500

    added option to either extract the largest connected component features or for all of them in MorphologicFeatures

--- src/applications/FeatureExtraction/src/depends/MorphologicFeatures.h
+++ src/applications/FeatureExtraction/src/depends/MorphologicFeatures.h
@@ -33,6 +33,11 @@ public:
   {
     m_maskShape = maskShape;
   }
+
+  void SetRange(int inputRange)
+  {
+    m_extractionType = inputRange;
+  }
   
   /**
   \brief Update calculate five feature values
@@ -41,21 +46,6 @@ public:
   {
     if (!this->m_algorithmDone)
     {
-      auto size = this->m_Mask->GetBufferedRegion().GetSize();
-      auto minSize = size[0];
-      auto maxSize = size[0];
-      for (size_t d = 1; d < TImageType::ImageDimension; d++)
-      {
-        if (size[d] < minSize)
-        {
-          minSize = size[d];
-        }
-        if (size[d] > maxSize)
-        {
-          maxSize = size[d];
-        }
-      }
-
       using LabelType = short;
       using OutputImageType = itk::Image< LabelType, 3 >;
       //auto connected = itk::ConnectedComponentImageFilter < TShapeImageType, OutputImageType >::New();
@@ -101,57 +91,112 @@ public:
             largestComponentSize = currentComponentSize;
           }
         }
+
+        auto labelObject = labelMap->GetNthLabelObject(componentToConsider);
+
+        auto ellipseDiameter = labelObject->GetEquivalentEllipsoidDiameter();
+        auto orientedBoundingBoxSize = labelObject->GetOrientedBoundingBoxSize();
+
+        double numerator = 1;
+        if (ellipseDiameter.Size() == 2)
+        {
+          numerator = std::pow(ellipseDiameter[0], 2);
+        }
+        else if (ellipseDiameter.Size() == 3)
+        {
+          numerator = ellipseDiameter[0] * ellipseDiameter[1];
+        }
+
+        this->m_features["LargestComponentSize"] = largestComponentSize;
+        this->m_features["Eccentricity"] = std::sqrt(1 - numerator / std::pow(ellipseDiameter[ellipseDiameter.Size() - 1], 2));
+
+        for (size_t d = 0; d < TShapeImageType::ImageDimension; d++)
+        {
+          this->m_features["EllipseDiameter_Axis-" + std::to_string(d)] = ellipseDiameter[d];
+          this->m_features["OrientedBoundingBoxSize_Axis-" + std::to_string(d)] = orientedBoundingBoxSize[d];
+
+          //std::cout << "EllipseDiameter" + "_Axis-" + std::to_string(d) << " = " << ellipseDiameter[d] << "\n";
+        }
+        //m_features["FeretDiameter"] = labelObject->GetFeretDiameter();
+        this->m_features["PerimeterOnBorder"] = labelObject->GetPerimeterOnBorder();
+        this->m_features["Perimeter"] = labelObject->GetPerimeter();
+        this->m_features["PixelsOnBorder"] = labelObject->GetNumberOfPixelsOnBorder();
+        this->m_features["PhysicalSize"] = labelObject->GetPhysicalSize();
+        this->m_features["NumberOfPixels"] = labelObject->GetNumberOfPixels();
+        this->m_features["EquivalentSphericalRadius"] = labelObject->GetEquivalentSphericalRadius();
+        this->m_features["EquivalentSphericalPerimeter"] = labelObject->GetEquivalentSphericalPerimeter();
+        this->m_features["PerimeterOnBorderRatio"] = labelObject->GetPerimeterOnBorderRatio();
+        this->m_features["Roundness"] = labelObject->GetRoundness();
+        this->m_features["Flatness"] = labelObject->GetFlatness();
+        this->m_features["Elongation"] = labelObject->GetElongation();
+        //m_features["OrientedBoundingBoxSize"] = labelObject->GetOrientedBoundingBoxSize(); // vector<double 2> cannot be converted to double
+
+        //std::cout << "Eccentricity" << " = " << this->m_features["Eccentricity"] << "\n";
+        //std::cout << "PerimeterOnBorder" << " = " << labelObject->GetPerimeterOnBorder() << "\n";
+        //std::cout << "Perimeter" << " = " << labelObject->GetPerimeter() << "\n";
+
       }
-
-      auto labelObject = labelMap->GetNthLabelObject(componentToConsider);
-
-      std::string labelString = ""; // TBD: redundant
-      //if (numberOfLabelObjects > 1)
-      //{
-      //  labelString = "-Label-" + std::to_string(i);
-      //}
-      /// TBD: convert this into a function of minSize or maxSize instead of the hard-coded '20'
-
-      auto ellipseDiameter = labelObject->GetEquivalentEllipsoidDiameter();
-      auto orientedBoundingBoxSize = labelObject->GetOrientedBoundingBoxSize();
-
-      double numerator = 1;
-      if (ellipseDiameter.Size() == 2)
+      else
       {
-        numerator = std::pow(ellipseDiameter[0], 2);
+        auto size = this->m_Mask->GetBufferedRegion().GetSize();
+        size_t totalSizeThreshold = 0.05; // 5% threshold in case all connected components are chosen
+        for (size_t d = 1; d < TImageType::ImageDimension; d++)
+        {
+          totalSizeThreshold *= size[d];
+        }
+
+        for (size_t i = 1; i < numberOfLabelObjects; i++) // 0 is always background
+        {
+          auto labelObject = labelMap->GetNthLabelObject(i);
+
+          std::string labelString = ""; // TBD: redundant
+          if (numberOfLabelObjects > 2)
+          {
+            labelString = "-Label-" + std::to_string(i);
+          }
+          auto currentComponentSize = labelMap->GetNthLabelObject(i)->GetNumberOfPixels();
+
+          if (currentComponentSize > totalSizeThreshold)
+          {
+            auto ellipseDiameter = labelObject->GetEquivalentEllipsoidDiameter();
+            auto orientedBoundingBoxSize = labelObject->GetOrientedBoundingBoxSize();
+
+            double numerator = 1;
+            if (ellipseDiameter.Size() == 2)
+            {
+              numerator = std::pow(ellipseDiameter[0], 2);
+            }
+            else if (ellipseDiameter.Size() == 3)
+            {
+              numerator = ellipseDiameter[0] * ellipseDiameter[1];
+            }
+
+            this->m_features["Eccentricity" + labelString] = std::sqrt(1 - numerator / std::pow(ellipseDiameter[ellipseDiameter.Size() - 1], 2));
+
+            for (size_t d = 0; d < TShapeImageType::ImageDimension; d++)
+            {
+              this->m_features["EllipseDiameter" + labelString + "_Axis-" + std::to_string(d)] = ellipseDiameter[d];
+              this->m_features["OrientedBoundingBoxSize" + labelString + "_Axis-" + std::to_string(d)] = orientedBoundingBoxSize[d];
+
+              //std::cout << "EllipseDiameter" + labelString + "_Axis-" + std::to_string(d) << " = " << ellipseDiameter[d] << "\n";
+            }
+            //m_features["FeretDiameter" + labelString] = labelObject->GetFeretDiameter();
+            this->m_features["PerimeterOnBorder" + labelString] = labelObject->GetPerimeterOnBorder();
+            this->m_features["Perimeter" + labelString] = labelObject->GetPerimeter();
+            this->m_features["PixelsOnBorder" + labelString] = labelObject->GetNumberOfPixelsOnBorder();
+            this->m_features["PhysicalSize" + labelString] = labelObject->GetPhysicalSize();
+            this->m_features["NumberOfPixels" + labelString] = labelObject->GetNumberOfPixels();
+            this->m_features["EquivalentSphericalRadius" + labelString] = labelObject->GetEquivalentSphericalRadius();
+            this->m_features["EquivalentSphericalPerimeter" + labelString] = labelObject->GetEquivalentSphericalPerimeter();
+            this->m_features["PerimeterOnBorderRatio" + labelString] = labelObject->GetPerimeterOnBorderRatio();
+            this->m_features["Roundness" + labelString] = labelObject->GetRoundness();
+            this->m_features["Flatness" + labelString] = labelObject->GetFlatness();
+            this->m_features["Elongation" + labelString] = labelObject->GetElongation();
+            //m_features["OrientedBoundingBoxSize"] = labelObject->GetOrientedBoundingBoxSize(); // vector<double 2> cannot be converted to double
+          }
+        }
+
       }
-      else if (ellipseDiameter.Size() == 3)
-      {
-        numerator = ellipseDiameter[0] * ellipseDiameter[1];
-      }
-
-      this->m_features["Eccentricity" + labelString] = std::sqrt(1 - numerator / std::pow(ellipseDiameter[ellipseDiameter.Size() - 1], 2));
-
-      for (size_t d = 0; d < TShapeImageType::ImageDimension; d++)
-      {
-        this->m_features["EllipseDiameter" + labelString + "_Axis-" + std::to_string(d)] = ellipseDiameter[d];
-        this->m_features["OrientedBoundingBoxSize" + labelString + "_Axis-" + std::to_string(d)] = orientedBoundingBoxSize[d];
-
-        //std::cout << "EllipseDiameter" + labelString + "_Axis-" + std::to_string(d) << " = " << ellipseDiameter[d] << "\n";
-      }
-      //m_features["FeretDiameter" + labelString] = labelObject->GetFeretDiameter();
-      this->m_features["PerimeterOnBorder" + labelString] = labelObject->GetPerimeterOnBorder();
-      this->m_features["Perimeter" + labelString] = labelObject->GetPerimeter();
-      this->m_features["PixelsOnBorder" + labelString] = labelObject->GetNumberOfPixelsOnBorder();
-      this->m_features["PhysicalSize" + labelString] = labelObject->GetPhysicalSize();
-      this->m_features["NumberOfPixels" + labelString] = labelObject->GetNumberOfPixels();
-      this->m_features["EquivalentSphericalRadius" + labelString] = labelObject->GetEquivalentSphericalRadius();
-      this->m_features["EquivalentSphericalPerimeter" + labelString] = labelObject->GetEquivalentSphericalPerimeter();
-      this->m_features["PerimeterOnBorderRatio" + labelString] = labelObject->GetPerimeterOnBorderRatio();
-      this->m_features["Roundness" + labelString] = labelObject->GetRoundness();
-      this->m_features["Flatness" + labelString] = labelObject->GetFlatness();
-      this->m_features["Elongation" + labelString] = labelObject->GetElongation();
-      //m_features["OrientedBoundingBoxSize"] = labelObject->GetOrientedBoundingBoxSize(); // vector<double 2> cannot be converted to double
-
-      //std::cout << "Eccentricity" + labelString << " = " << this->m_features["Eccentricity" + labelString] << "\n";
-      //std::cout << "PerimeterOnBorder" + labelString << " = " << labelObject->GetPerimeterOnBorder() << "\n";
-      //std::cout << "Perimeter" + labelString << " = " << labelObject->GetPerimeter() << "\n";
-
 
       this->m_algorithmDone = true;
     }
