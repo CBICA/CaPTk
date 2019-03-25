808802ede53e46759e87acc840a3a1f8ec32ced8
surrsurus <surrsurus@protonmail.com>
2019-03-25 13:37:13 -0400

    Add linux fix

---
+++ docs/navtreedata.js
@@ -0,0 +1,22 @@
+var NAVTREE =
+[
+  [ "Cancer Imaging Phenomics Toolkit (CaPTk)", "index.html", [
+    [ "Overview", "index.html", null ],
+    [ "Installation Instructions", "Installation.html", null ],
+    [ "Getting Started", "Getting_Started.html", "Getting_Started" ],
+    [ "How To Guides", "How_To_Guides.html", "How_To_Guides" ],
+    [ "Scientific Findings using CaPTk", "Science.html", "Science" ],
+    [ "Technical Reference", "Technical_Reference.html", "Technical_Reference" ],
+    [ "Download Instructions", "Download.html", null ],
+    [ "Release Notes", "ReleaseNotes.html", null ],
+    [ "People (Credits)", "People.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"BreastCancer_LIBRA.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
