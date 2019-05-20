3335359c2cb03fb48b7d696976afe32d74e67002
PhucNgo1711 <phucnmrot@gmail.com>
2019-05-20 15:00:53 -0400

    fix glx

--- src/CaPTk.cxx
+++ src/CaPTk.cxx
@@ -167,9 +167,9 @@ int main(int argc, char** argv)
   }
   
 #if defined(__linux__)
-  auto defaultFormat = QVTKOpenGLWidget::defaultFormat();
-  defaultFormat.setSamples(0);
-  // defaultFormat.setVersion(3, 0);
+  QSurfaceFormat defaultFormat;
+  // defaultFormat.setSamples(0);
+  defaultFormat.setVersion(3, 0);
   QSurfaceFormat::setDefaultFormat(defaultFormat);
 #else
   QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
