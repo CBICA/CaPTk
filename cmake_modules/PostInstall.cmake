5322dd1bf2481cb3a54282376e13bfcb991ecb1e
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-06-28 19:02:57 -0400

    checking post installation using the PostInstall.cmake

--- cmake_modules/PostInstall.cmake
+++ cmake_modules/PostInstall.cmake
@@ -1,5 +1,5 @@
 # Add symlink on osx and linux
-
-IF (LINUX)
-    INSTALL(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/bin/${EXE_NAME} ${CMAKE_INSTALL_PREFIX}/bin/captk)")
+IF (NOT WIN32)
+  MESSAGE("Adding a symlink")
+  INSTALL(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/bin/${EXE_NAME} {CMAKE_INSTALL_PREFIX}/bin/captk)")
 ENDIF()
