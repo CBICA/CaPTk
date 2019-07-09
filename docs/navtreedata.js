449788a39ed08643fe56331fdb380985dffe66b5
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-07-09 16:54:22 -0400

    how to guide has been converted to a proper strucutre

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -27,7 +27,52 @@ var NAVTREE =
     [ "Overview", "index.html", null ],
     [ "Installation Instructions", "Installation.html", null ],
     [ "Getting Started", "Getting_Started.html", "Getting_Started" ],
-    [ "How To Guides", "How_To_Guides.html", "How_To_Guides" ],
+    [ "How To Guides", "How_To_Guides.html", [
+      [ "Pre-processing", "How_To_Guides.html#ht_Preprocessing", [
+        [ "DICOM to NIfTI conversion", "How_To_Guides.html#preprocessing_dcm2nii", null ],
+        [ "Image Co-registration", "How_To_Guides.html#preprocessing_reg", null ],
+        [ "Denoise-SUSAN (ITK filter)", "How_To_Guides.html#preprocessing_susan", null ],
+        [ "N4 Bias Correction (ITK filter)", "How_To_Guides.html#preprocessing_biasN4", null ],
+        [ "Histogram Matching", "How_To_Guides.html#preprocessing_histoMatch", null ],
+        [ "Z-Scoring Normalization", "How_To_Guides.html#preprocessing_zScoreNorm", null ]
+      ] ],
+      [ "Segmentation", "How_To_Guides.html#ht_Segmentation", [
+        [ "Geodesic Training Segmentation", "How_To_Guides.html#seg_GeoTrain", null ],
+        [ "Geodesic Distance Transform-based Segmentation", "How_To_Guides.html#seg_Geodesic", null ],
+        [ "ITK-SNAP", "How_To_Guides.html#seg_SNAP", null ],
+        [ "Deep Learning Segmentation", "How_To_Guides.html#seg_DL", null ]
+      ] ],
+      [ "Feature Extraction", "How_To_Guides.html#ht_FeatureExtraction", null ],
+      [ "Specialized Applications (SAs) Usage", "How_To_Guides.html#ht_SpecialApps", [
+        [ "Brain Cancer", "How_To_Guides.html#Glioblastoma", [
+          [ "Glioblastoma Molecular Subtype Prediction", "How_To_Guides.html#Glioblastoma_Molecular", null ],
+          [ "WhiteStripe Normalization", "How_To_Guides.html#Glioblastoma_WhiteStripe", null ],
+          [ "Glioblastoma EGFRvIII Surrogate Index (PHI Estimator)", "How_To_Guides.html#Glioblastoma_PHI", null ],
+          [ "Glioblastoma Infiltration Index (Recurrence)", "How_To_Guides.html#Glioblastoma_Recurrence", null ],
+          [ "Glioblastoma Survival Prediction Index", "How_To_Guides.html#Glioblastoma_Survival", null ],
+          [ "Glioblastoma EGFRvIII SVM Index", "How_To_Guides.html#Glioblastoma_EGFRvIII", null ],
+          [ "Pseudoprogression Infiltration Index", "How_To_Guides.html#Glioblastoma_Pseudoprogression", null ],
+          [ "Population Atlas", "How_To_Guides.html#Glioblastoma_Atlas", null ],
+          [ "Confetti", "How_To_Guides.html#Glioblastoma_Confetti", null ],
+          [ "Directionality Estimator", "How_To_Guides.html#Glioblastoma_Directionality", null ]
+        ] ],
+        [ "Breast Cancer", "How_To_Guides.html#BreastCancer", [
+          [ "Breast Density Estimation (LIBRA)", "How_To_Guides.html#BreastCancer_LIBRA", null ],
+          [ "Texture Feature Extraction", "How_To_Guides.html#BreastCancer_texture", null ],
+          [ "Breast Segmentation", "How_To_Guides.html#BreastCancer_breastSegmentation", null ]
+        ] ],
+        [ "Lung Cancer", "How_To_Guides.html#LungCancer", [
+          [ "Radiomics Analysis of Lung Cancer", "How_To_Guides.html#LungCancer_SBRT", null ]
+        ] ],
+        [ "Miscellaneous Applications", "How_To_Guides.html#Miscellaneous", [
+          [ "Perfusion Alignment", "How_To_Guides.html#misc_Perfusion_Alignment", null ],
+          [ "Perfusion Derivatives", "How_To_Guides.html#misc_Perfusion_Derivatives", null ],
+          [ "Diffusion Derivatives", "How_To_Guides.html#misc_Diffusion_Derivatives", null ],
+          [ "PCA Volume Extraction", "How_To_Guides.html#misc_PCA_Extraction", null ],
+          [ "Training Module", "How_To_Guides.html#misc_Training_Module", null ]
+        ] ]
+      ] ]
+    ] ],
     [ "Scientific Findings using CaPTk", "Science.html", [
       [ "Non-invasive Imaging Biomarker of EGFRvIII in Glioblastoma Patients", "Science.html#phiEstimator", null ],
       [ "Prediction of Overall Survival in Glioblastoma Patients", "Science.html#survivalPredictor", null ],
@@ -60,7 +105,7 @@ var NAVTREE =
 
 var NAVTREEINDEX =
 [
-"BreastCancer_LIBRA.html"
+"Download.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
