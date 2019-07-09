3b37b44d3bb07fcd4e7b2dc7c5ff85b5550308cc
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-07-09 17:14:30 -0400

    getting started is also getting the same treatment. now all table of content information is handled the same way

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -26,7 +26,34 @@ var NAVTREE =
   [ "Cancer Imaging Phenomics Toolkit (CaPTk)", "index.html", [
     [ "Overview", "index.html", null ],
     [ "Installation Instructions", "Installation.html", null ],
-    [ "Getting Started", "Getting_Started.html", "Getting_Started" ],
+    [ "Getting Started", "Getting_Started.html", [
+      [ "Frequently Asked Questions", "Getting_Started.html#gs_FAQ", null ],
+      [ "Keyboard Shortcuts", "Getting_Started.html#gs_keyboard", null ],
+      [ "Components", "Getting_Started.html#gs_components", null ],
+      [ "Supported Images", "Getting_Started.html#gs_supportedImages", null ],
+      [ "Image Loading", "Getting_Started.html#gs_imageLoading", null ],
+      [ "Image Visualization", "Getting_Started.html#gs_imageVisualization", null ],
+      [ "Tab Docking (Windows-only)", "Getting_Started.html#gs_tabDocking", null ],
+      [ "Utilities (Command-line only)", "Getting_Started.html#gs_utilities", null ],
+      [ "Pre-processing", "Getting_Started.html#gs_preprocessing", null ],
+      [ "Coordinate definition (Seed-point initialization)", "Getting_Started.html#gs_seedpoints", null ],
+      [ "Label Annotation/Drawing Panel", "Getting_Started.html#gs_drawing", null ],
+      [ "Segmentation", "Getting_Started.html#gs_segmentation", null ],
+      [ "Feature Extraction", "Getting_Started.html#gs_features", null ],
+      [ "Specialized Applications", "Getting_Started.html#gs_specializedApps", null ],
+      [ "Command Line Usage", "Getting_Started.html#gs_commandLineUsage", null ]
+    ] ],
+    [ "Frequently Asked Questions", "FAQ.html", [
+      [ "Which platforms are supported by CaPTk installers?", "FAQ.html#gs_FAQ_1", null ],
+      [ "How do I run CaPTk Applications from the Command Line?", "FAQ.html#gs_FAQ_2", null ],
+      [ "What are the OpenGL requirements to run CaPTk?", "FAQ.html#gs_FAQ_3", null ],
+      [ "What if I am having issues with the Linux Installer?", "FAQ.html#gs_FAQ_4", [
+        [ "FUSE Issues", "FAQ.html#gs_FAQ_4_1", null ],
+        [ "GLIBCXX or GLIBC Issues", "FAQ.html#gs_FAQ_4_2", null ]
+      ] ],
+      [ "Is there a minimum recommended screen resolution for running CaPTk?", "FAQ.html#gs_FAQ_5", null ],
+      [ "What if I am having an issue not listed here?", "FAQ.html#gs_FAQ_6", null ]
+    ] ],
     [ "How To Guides", "How_To_Guides.html", [
       [ "Pre-processing", "How_To_Guides.html#ht_Preprocessing", [
         [ "DICOM to NIfTI conversion", "How_To_Guides.html#preprocessing_dcm2nii", null ],
