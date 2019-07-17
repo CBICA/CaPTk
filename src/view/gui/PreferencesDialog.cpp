b62fc3a8809b68137134554c0dbb49dbb0e1e4cf
Ashish Singh <ashish.singh@uphs.upenn.edu>
2019-07-17 14:11:43 -0400

    linux build fix

--- src/view/gui/PreferencesDialog.cpp
+++ src/view/gui/PreferencesDialog.cpp
@@ -1,4 +1,17 @@
-#include "preferencesdialog.h"
+/**
+\file  PreferencesDialog.cpp
+
+\brief Implementation of PreferencesDialog class
+
+https://www.med.upenn.edu/sbia/software/ <br>
+software@cbica.upenn.edu
+
+Copyright (c) 2018 University of Pennsylvania. All rights reserved. <br>
+See COPYING file or https://www.med.upenn.edu/sbia/software-agreement.html
+
+*/
+
+#include "PreferencesDialog.h"
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
