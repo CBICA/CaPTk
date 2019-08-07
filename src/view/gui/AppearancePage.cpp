5428157d3d6034423288960ee86e2f90f6e79c56
Ashish Singh <ashish.singh@uphs.upenn.edu>
2019-08-07 15:06:51 -0400

    adding license/copyright block, #595

--- src/view/gui/AppearancePage.cpp
+++ src/view/gui/AppearancePage.cpp
@@ -1,3 +1,15 @@
+/**
+\file  AppearancePage.cpp
+
+\brief Definition of AppearancePage class
+
+https://www.med.upenn.edu/sbia/software/ <br>
+software@cbica.upenn.edu
+
+Copyright (c) 2018 University of Pennsylvania. All rights reserved. <br>
+See COPYING file or https://www.med.upenn.edu/sbia/software-agreement.html
+
+*/
 #include "AppearancePage.h"
 #include "ui_AppearancePage.h"
 
--- src/view/gui/AppearancePage.h
+++ src/view/gui/AppearancePage.h
@@ -1,3 +1,15 @@
+/**
+\file  AppearancePage.h
+
+\brief Declaration of AppearancePage class
+
+https://www.med.upenn.edu/sbia/software/ <br>
+software@cbica.upenn.edu
+
+Copyright (c) 2018 University of Pennsylvania. All rights reserved. <br>
+See COPYING file or https://www.med.upenn.edu/sbia/software-agreement.html
+
+*/
 #ifndef APPEARANCEPAGE_H
 #define APPEARANCEPAGE_H
 
