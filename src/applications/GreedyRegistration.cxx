f3beb7ebfb7199216b4f4a45a63cc1931e3078f4
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-10-01 13:35:26 +0200

    redundant stuff removed

--- src/applications/GreedyRegistration.cxx
+++ src/applications/GreedyRegistration.cxx
@@ -22,7 +22,6 @@ See COPYING file or https://www.cbica.upenn.edu/sbia/software/license.html
 #include "cbicaUtilities.h"
 #include "cbicaITKSafeImageIO.h"
 
-/// original main function
 int main(int argc, char** argv)
 {
   std::string argv_common; // this is where we will be putting stuff for Preprocessing -reg
@@ -50,7 +49,7 @@ int main(int argc, char** argv)
   parser.addOptionalParameter("th", "threads", cbica::Parameter::INTEGER, "none", "Number of threads for algorithm", "IGNORED");
   //parser.exampleUsage("-reg -trf -i moving.nii.gz -f fixed.nii.gz -o output.nii.gz -t matrix.mat -a -m MI -n 100x50x5 -th 4");
 
-  parser.addApplicationDescription("This does affine registration based on Greedy");
+  parser.addApplicationDescription("This does affine registration based on Greedy; calls 'Preprocessing -reg' internally");
   parser.addExampleUsage("-reg -trf -i moving.nii.gz -f fixed.nii.gz -o output.nii.gz -t matrix.mat -a -m MI -n 100x50x5",
     "This registers the moving image 'moving.nii.gz' with fixed image 'fixed.nii.gz.' with output at 'output.nii.gz'");
 
@@ -210,44 +209,4 @@ int main(int argc, char** argv)
     return std::system(commandToRun.c_str());
   }
 
-}
-///
-
-
-int main(int argc, char** argv)
-{
-  cbica::CmdParser parser(argc, argv, "GreedyRegistration");
-
-  parser.addRequiredParameter("i", "movingImage", cbica::Parameter::FILE, "NIfTI", "Input Image for processing", "Becomes moving image in Registration mode");
-  parser.addRequiredParameter("o", "outputImage", cbica::Parameter::FILE, "NIfTI", "Output Image for processing");
-  parser.addOptionalParameter("m", "maskImage", cbica::Parameter::FILE, "NIfTI", "Input Mask for processing");
-  
-  parser.addOptionalParameter("reg", "registration", cbica::Parameter::STRING, "Affine | Deformable", "The kind of registration to perform", "Can use Mask File");
-  parser.addOptionalParameter("rFI", "regFixedImg", cbica::Parameter::FILE, "NIfTI", "The Fixed Image for the registration", "Needed for registration");
-  parser.addOptionalParameter("rME", "regMetrics", cbica::Parameter::STRING, "SSD | MI | NMI | NCC-AxBxC", "The kind of metris to use: SSD (Sum of Squared Differences) or MI (Mutual Information) or", "NMI (Normalized Mutual Information) or NCC-AxBxC (Normalized Cross correlation with integer radius for 3D image)");
-  parser.addOptionalParameter("rNI", "regNoIters", cbica::Parameter::STRING, "N1,N2,N3", "The umber of iterations per level of multi-res");
-  parser.addOptionalParameter("rIS", "regInterSave", cbica::Parameter::BOOLEAN, "0 or 1", "Whether the intermediate files are to be saved or not");
-  parser.addOptionalParameter("rSg", "regSegMoving", cbica::Parameter::BOOLEAN, "0 or 1", "Whether the Moving Image is a segmentation file", "If 1, the 'Nearest Label' Interpolation is applied");
-  parser.addOptionalParameter("rIA", "regInterAffn", cbica::Parameter::FILE, "mat", "The path to the affine transformation to apply to moving image", "If this is present, the Affine registration step will be skipped");
-  parser.addOptionalParameter("rID", "regInterDefm", cbica::Parameter::FILE, "NIfTI", "The path to the deformable transformation to apply to moving image", "If this is present, the Deformable registration step will be skipped");
-
-  parser.addApplicationDescription("This does affine registration based on Greedy and internally calls Preprocessing; run 'Preprocessing -h' for details");
-  parser.addExampleUsage("-i moving.nii.gz -rFI fixed.nii.gz -o output.nii.gz -reg Affine -rME NCC-2x2x2 -rIS 1 -rNI 100,50,5",
-    "This registers the moving image affinely 'moving.nii.gz' with fixed image 'fixed.nii.gz.' with output at 'output.nii.gz' using NCC metric with kernel size 2x2x2");
-
-  std::string argv_common;
-  if (argc > 1)
-  {
-    for (size_t i = 1; i < argc; i++)
-    {
-      argv_common += " " + std::string(argv[i]);
-    }
-  }
-  auto commandToRun = cbica::getExecutablePath() + "/Preprocessing" + 
-#if WIN32
-    ".exe" +
-#endif
-    argv_common;
-
-  return std::system(commandToRun.c_str());
 }
