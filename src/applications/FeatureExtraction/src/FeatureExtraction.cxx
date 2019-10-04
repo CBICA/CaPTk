8b0a551f9414f70411c0742c4290b8df301d755b
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-10-04 15:13:15 -0400

    started addressing #647

--- src/applications/FeatureExtraction/src/FeatureExtraction.cxx
+++ src/applications/FeatureExtraction/src/FeatureExtraction.cxx
@@ -204,7 +204,7 @@ int main(int argc, char** argv)
   parser.addOptionalParameter("p", "paramFile", cbica::Parameter::FILE, ".csv", "A csv file with all features and its parameters filled", "Default: '../data/1_params_default.csv'");
   parser.addRequiredParameter("o", "outputDir", cbica::Parameter::DIRECTORY, "none", "Absolute path of directory to save results", "Result can be a CSV or Feature Maps (for lattice)");
 
-  parser.addOptionalParameter("b", "batchFile", cbica::Parameter::FILE, ".csv", "Input file with Multi-Patient Multi-Modality details", "Header format is as follows:", "'PATIENT_ID,IMAGES,MASK,ROI,SELECTED_ROI,ROI_LABEL,", "SELECTED_FEATURES,PARAM_FILE'", "Delineate individual fields by '|'");
+  parser.addOptionalParameter("b", "batchFile", cbica::Parameter::FILE, ".csv", "Input file with Multi-Patient Multi-Modality details", "Example: '${CaPTk_InstallDir}/share/featureExtractionBatch/batch_featureExtraction.csv'");
 
   parser.addOptionalParameter("n", "name_patient", cbica::Parameter::STRING, "none", "Patient id", "Required for single subject mode");
   parser.addOptionalParameter("i", "imagePaths", cbica::Parameter::STRING, "none", "Absolute path of each coregistered modality", "Delineate by ','", "Example: -i c:/test1.nii.gz,c:/test2.nii.gz", "Required for single subject mode");
