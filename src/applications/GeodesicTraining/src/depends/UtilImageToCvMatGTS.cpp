bd63c6c5c8d3e31efe4083704d5d2bbc1a5dd84b
Dimitris Bounias <bounias.dimitris@gmail.com>
2019-10-31 10:18:36 -0400

    Fix Geodesic Training linker error in v0.15

--- src/applications/GeodesicTraining/src/depends/UtilImageToCvMatGTS.cpp
+++ src/applications/GeodesicTraining/src/depends/UtilImageToCvMatGTS.cpp
@@ -1,1 +1,9 @@
 #include "UtilImageToCvMatGTS.h"
+
+void GeodesicTrainingSegmentation::ParserGTS::ScaleSomeOfTheColumns(cv::Mat& mat, int colStart, int colEnd, double ratio)
+{
+	for (int i_col = colStart; i_col <= colEnd; i_col++)
+	{
+		mat.col(i_col).convertTo(mat.col(i_col), CV_32F, ratio);
+	}
+}
