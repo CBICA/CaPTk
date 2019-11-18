333c656586d400ae57ec3861c7861e389faaa6f9
PhucNgo1711 <phucnmrot@gmail.com>
2019-11-18 12:56:02 -0500

    Test self host again

--- cmake_modules/ChangeDylib.cmake
+++ cmake_modules/ChangeDylib.cmake
@@ -6,7 +6,7 @@ endfunction()
 macro(changeDylib target application libname libpath)
   add_custom_command(TARGET ${target} POST_BUILD
   COMMAND 
-  sudo install_name_tool -change \"${libpath}/${libname}\" \"@executable_path/../../Frameworks/${libname}\" \"${CMAKE_BINARY_DIR}/${application}\"
+  install_name_tool -change \"${libpath}/${libname}\" \"@executable_path/../../Frameworks/${libname}\" \"${CMAKE_BINARY_DIR}/${application}\"
 
   COMMENT "Change ${libname} dylib path ${application}"
   )
