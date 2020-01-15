440d64d25be563b6c60a7ddc636601c0893de6a1
Dimitris Bounias <bounias.dimitris@gmail.com>
2020-01-15 15:42:00 -0500

    Avoid dependency issues

--- src/common/inc/CaPTkUtils.h
+++ src/common/inc/CaPTkUtils.h
@@ -214,10 +214,56 @@ inline bool isExtensionSupported(const std::string inputExtension)
 }
 
 /** \brief Find total available memory (based on StackOverflow 2513505, Travis Gockel answer) */
-unsigned long long getTotalInstalledMemory();
+inline unsigned long long getTotalInstalledMemory()
+{
+#ifdef _WIN32
+  MEMORYSTATUSEX status;
+  status.dwLength = sizeof(status);
+  GlobalMemoryStatusEx(&status);
+  return status.ullTotalPhys;
+#else
+  long pages = sysconf(_SC_PHYS_PAGES);
+  long page_size = sysconf(_SC_PAGE_SIZE);
+  return pages * page_size;
+#endif
+}
 
 /** \brief Check if the total size of the files is more than a 
  * percentage of the available memory
  * */
-bool isSizeOfLoadedFilesTooBig(QStringList files, std::string loggerFile = "", 
-                                      float maxPercentage = 0.05);
+inline bool isSizeOfLoadedFilesTooBig(QStringList files, std::string loggerFile = "", 
+                               float maxPercentage = 0.05)
+{
+  // Find total size of all files
+  unsigned long long imagesSize = 0;
+  for (QString& file : files)
+  {
+    QFile qFile(file);
+    if (qFile.open(QIODevice::ReadOnly)){
+        imagesSize += qFile.size();  //when file does open. This size is in bytes
+        qFile.close();
+    }
+  }
+
+  /**** Get total amount of ram ****/
+  unsigned long long availableMemory = getTotalInstalledMemory();
+
+  // Log values
+  if (loggerFile != "")
+  {
+    cbica::Logging(loggerFile, 
+      "Images size: " + std::to_string(imagesSize));
+    cbica::Logging(loggerFile, 
+      "Total RAM: " + std::to_string(availableMemory));
+  }
+
+  // Compare (maxPercentage is arbitrary, default=0.05 it allows images up to 400MB for a 8GB system)
+  if (imagesSize > maxPercentage*availableMemory)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
