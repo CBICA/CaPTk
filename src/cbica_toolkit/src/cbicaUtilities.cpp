5666c37cf21b061ce78c8be06fe11ddc6aeddcc1
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2020-02-24 08:39:14 -0500

    compile fix

--- src/cbica_toolkit/src/cbicaUtilities.cpp
+++ src/cbica_toolkit/src/cbicaUtilities.cpp
@@ -752,7 +752,7 @@ namespace cbica
      char path[PATH_MAX];
      if (::readlink("/proc/self/exe", path, sizeof(path) - 1) == -1)
        std::cerr << "[getFullPath()] Error during getting full path..";
-    std::string appPath = std::string( result, (count > 0) ? count : 0 );
+    std::string appPath = std::string( path, (count > 0) ? count : 0 );
     std::cerr << "appPath: " << appPath << "\n";
 #endif
 
