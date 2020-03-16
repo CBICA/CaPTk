9bd891c23bb5f7b9aeb79bb8e2c1fc3351f4756d
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2020-03-16 11:29:24 -0400

    member variable to control the bin range

--- src/applications/FeatureExtraction/src/FeatureExtraction.h
+++ src/applications/FeatureExtraction/src/FeatureExtraction.h
@@ -769,6 +769,7 @@ private:
 
   // the parameters that keep changing on a per-feature basis
   int m_Radius = 0, m_Bins = 0, m_Dimension = 0, m_Direction = 0, m_neighborhood = 0, m_LBPStyle = 0;
+  std::vector< int > m_Bins_range; //! testing
   float m_Radius_float = 0.0, m_Range = 0, 
     m_Bins_min = std::numeric_limits<float>::max(); //! the starting index of the histogram binning
   std::string m_Axis, m_offsetSelect; //! these are string based parameters
