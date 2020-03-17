bf2b068db87c9bd4ec3c88b22e33d4d69cb12976
PhucNgo1711 <phucnmrot@gmail.com>
2020-03-17 19:59:12 -0400

    remove dependency libzip

--- src/view/gui/fMainWindow.cpp
+++ src/view/gui/fMainWindow.cpp
@@ -1201,33 +1201,33 @@ void fMainWindow::appDownload(std::string currentApp)
     
 }
 
-void fMainWindow::unzipArchive(std::string fullPath) 
-{
-  // //Open the ZIP archive
-  // int err = 0;
-  // zip *z = zip_open("fullPath", 0, &err);
+// void fMainWindow::unzipArchive(std::string fullPath) 
+// {
+//   // //Open the ZIP archive
+//   // int err = 0;
+//   // zip *z = zip_open("fullPath", 0, &err);
 
-  // //Search for the file of given name
-  // const char *name = "file.txt";
-  // struct zip_stat st;
-  // zip_stat_init(&st);
-  // zip_stat(z, name, 0, &st);
+//   // //Search for the file of given name
+//   // const char *name = "file.txt";
+//   // struct zip_stat st;
+//   // zip_stat_init(&st);
+//   // zip_stat(z, name, 0, &st);
 
-  // //Alloc memory for its uncompressed contents
-  // char *contents = new char[st.size];
+//   // //Alloc memory for its uncompressed contents
+//   // char *contents = new char[st.size];
 
-  // //Read the compressed file
-  // zip_file *f = zip_fopen(z, name, 0);
-  // zip_fread(f, contents, st.size);
-  // zip_fclose(f);
+//   // //Read the compressed file
+//   // zip_file *f = zip_fopen(z, name, 0);
+//   // zip_fread(f, contents, st.size);
+//   // zip_fclose(f);
 
-  // //And close the archive
-  // zip_close(z);
+//   // //And close the archive
+//   // zip_close(z);
 
-  // //Do something with the contents
-  // //delete allocated memory
-  // delete[] contents;
-}
+//   // //Do something with the contents
+//   // //delete allocated memory
+//   // delete[] contents;
+// }
 
 void fMainWindow::help_BugTracker()
 {
