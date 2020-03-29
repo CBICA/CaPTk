0b8d9d4a605fb8f64d80f2c6ac251b4c2607b51b
Saima Rathore <saima.rathore@pennmedicine.upenn.edu>
2020-03-29 00:42:03 -0400

    reference paper and comments added in the header file

--- src/applications/PopulationAtlases.h
+++ src/applications/PopulationAtlases.h
@@ -1,7 +1,7 @@
 /**
 \file  PopulationAtlases.h
 
-\brief The header file containing the SurvivaPredictor class, used to find Survival Prediction Index
+\brief The header file containing the PopulationAtlases class, used to calculate spatial distribution atlases and features
 Library Dependencies: ITK 4.7+ <br>
 
 https://www.med.upenn.edu/sbia/software/ <br>
@@ -16,7 +16,6 @@ See COPYING file or https://www.med.upenn.edu/sbia/software-agreement.html
 #ifndef _PopulationAtlases_h_
 #define _PopulationAtlases_h_
 
-//#include "CAPTk.h"
 #include "NiftiDataManager.h"
 #include "FeatureReductionClass.h"
 #include "FeatureScalingClass.h"
@@ -38,19 +37,17 @@ typedef vnl_matrix<double> MatrixType;
 /**
 \class PopulationAtlases
 
-\brief Calculates Survival Prediction Index
+\brief Spatial distribution atlases and features
 
 Reference:
 
-@article{macyszyn2015imaging,
-title={Imaging patterns predict patient survival and molecular subtype in glioblastoma via machine learning techniques},
-author={Macyszyn, Luke and Akbari, Hamed and Pisapia, Jared M and Da, Xiao and Attiah, Mark and Pigrish, Vadim and Bi, Yingtao and Pal, Sharmistha and Davuluri, Ramana V and Roccograndi, Laura and others},
-journal={Neuro-oncology},
-volume={18},
-number={3},
-pages={417--425},
-year={2015},
-publisher={Society for Neuro-Oncology}
+Reference:
+@article{Bilello.2016,
+title={Population-based MRI atlases of spatial distribution are specific to patient and tumor characteristics in glioblastoma},
+author={M.Bilello, H.Akbari, X. Da, J.M.Pisapia, S.Mohan, R.L.Wolf, D.M.O’Rourke, M.Martinez-Lage, C.Davatzikosa},
+journal={Neuroimage:Clinical},
+pages={34-40},
+year={2016}
 }
 
 */
@@ -60,133 +57,76 @@ class PopulationAtlases
 #endif
 {
 public:
-	//! Default constructor
-	PopulationAtlases()
-	{
-		mLastErrorMessage = "";
-	}
+  //! Default constructor
+  PopulationAtlases()
+  {
+    mLastErrorMessage = "";
+  }
 
-	//! Default destructor
-	~PopulationAtlases() {};
+  //! Default destructor
+  ~PopulationAtlases() {};
 
-	cbica::Logging logger;
-	std::string mLastErrorMessage;
+  cbica::Logging logger;
+  std::string mLastErrorMessage;
 
-	/**
-	\brief Calculates the spatial location features for given tumor images
+  /**
+  \brief Calculates the spatial distribution atlases using the given tumor images
 
-	\param labelImagePointer		Pointer to image having segmentation labels in patient space
-	\param atlasImagePointer		Pointer to image having segmentation labels in atlas space
-	*/
-	
-	std::vector<typename ImageType::Pointer> GeneratePopualtionAtlas(const std::vector<std::string>, const std::vector<int>, const std::string, const int no_of_atlases, const std::string);
-	
-  bool CalculateSpatialLocationFeatures(const std::vector<std::string> image_paths,
+  \param image_paths      		A list containing paths to the segmentation images
+  \param atlas_labels      		A list containing atlas number for each segmentation image
+  \param atlas_file       		Path of the standard atlas image
+  \param no_of_atlas       		Number of atlases specified by the users
+  \param output_directory     Path of the output directory
+  */
+  std::vector<typename ImageType::Pointer> GeneratePopualtionAtlas(const std::vector<std::string> image_paths,
     const std::vector<int> atlas_labels,
-    const std::string inputatlas,
+    const std::string atlas_file,
+    const int no_of_atlases, const std::string output_directory);
+
+  /**
+  \brief Calculates the spatial location features for given tumor images
+
+  \param image_paths      		A list containing paths to the segmentation images
+  \param atlas_file       		Path of the standard atlas image
+  \param number_of_regions		Number of regions in the atlas space image
+  \param AllLocationFeatures  A variable size matrix to store spatial distribution features
+  \param output_directory     Path of the output directory
+  */
+  bool CalculateSpatialLocationFeatures(const std::vector<std::string> image_paths,
+    const std::string atlas_file,
     const int number_of_regions,
     VariableSizeMatrixType & AllLocationFeatures,
-    const std::string outputdirectory);
-  
-  template <class ImageType>
-	typename ImageType::Pointer ReadNiftiImage(const std::string &filename);
-	
+    const std::string output_directory);
 
+  /**
+  \brief Removes connected components smaller than a certain area threshold
+
+  \param labelImagePointer    Pointer to the input segmentation image
+  \param areathreshold       	Area threshold used to remove smaller components
+  */
   template<class ImageType>
   std::vector<typename ImageType::Pointer> RevisedTumorArea(const typename ImageType::Pointer &labelImagePointer, const int areathreshold);
 
+  /**
+  \brief Calculates the spatial location features for each given tumor image
+  \param labelImagePointer            Pointer to the input segmentation image
+  \param jacobtemplateImagePointer    Pointer to the atlas image
+  \param numberofregions              Number of regions in the atlas space image
+  */
   template<class ImageType>
   VectorDouble GetSpatialLocationFeaturesForFixedNumberOfRegions(const typename ImageType::Pointer &labelImagePointer,
     const typename ImageType::Pointer &jacobtemplateImagePointer,
     const int numberofregions);
 
   void Run()
-	{
+  {
 
-	}
+  }
 
 private:
 
 };
 
-//template<class ImageType>
-//VectorDouble PopulationAtlases::GetSpatialLocationFeatures(const typename ImageType::Pointer &labelImagePointer, const typename ImageType::Pointer &jacobtemplateImagePointer)
-//{
-//	std::vector<typename ImageType::Pointer> RevisedImages = RevisedTumorArea<ImageType>(labelImagePointer);
-//	typename ImageType::Pointer tumorImage = RevisedImages[0];
-//	typename ImageType::Pointer etumorImage = RevisedImages[1];
-//	typename ImageType::Pointer ncrImage = RevisedImages[2];
-//
-//
-//	typename ImageType::Pointer localizeImage = ImageType::New();
-//	localizeImage->CopyInformation(labelImagePointer);
-//	localizeImage->SetRequestedRegion(labelImagePointer->GetLargestPossibleRegion());
-//	localizeImage->SetBufferedRegion(labelImagePointer->GetBufferedRegion());
-//	localizeImage->Allocate();
-//	localizeImage->FillBuffer(0);
-//
-//	//mulitply revised tumor image with the atlas ROI image
-//	typedef itk::ImageRegionIteratorWithIndex <ImageType> IteratorType;
-//	IteratorType tumorIt(tumorImage, tumorImage->GetLargestPossibleRegion());
-//	IteratorType localizeIt(localizeImage, localizeImage->GetLargestPossibleRegion());
-//	IteratorType atlasIt(jacobtemplateImagePointer, jacobtemplateImagePointer->GetLargestPossibleRegion());
-//
-//	atlasIt.GoToBegin();
-//	tumorIt.GoToBegin();
-//	localizeIt.GoToBegin();
-//
-//	while (!tumorIt.IsAtEnd())
-//	{
-//		localizeIt.Set(tumorIt.Get()*atlasIt.Get());
-//		++localizeIt;
-//		++tumorIt;
-//		++atlasIt;
-//	}
-//	//find number of voxels in 9 ROIs
-//	VectorDouble location;
-//	int tumorSize = 0;
-//	for (unsigned int i = 0; i < 9; i++)
-//	{
-//		int counter = 0;
-//		localizeIt.GoToBegin();
-//		while (!localizeIt.IsAtEnd())
-//		{
-//			if (localizeIt.Get() == i + 1)
-//			{
-//				counter++;
-//				tumorSize++;
-//			}
-//			++localizeIt;
-//		}
-//		location.push_back(counter);
-//	}
-//	//find percentages in 9 ROIs
-//	for (unsigned int i = 0; i < 9; i++)
-//		location[i] = (location[i] * 100) / tumorSize;
-//
-//	return location;
-//}
-
-
-template <class ImageType>
-typename ImageType::Pointer PopulationAtlases::ReadNiftiImage(const std::string &filename)
-{
-	typedef itk::ImageFileReader<ImageType> ImageReaderType;
-	typename ImageReaderType::Pointer reader = ImageReaderType::New();
-	reader->SetFileName(filename);
-
-	try
-	{
-		reader->Update();
-	}
-	catch (itk::ExceptionObject& e)
-	{
-		std::cerr << "Error caught: " << e.what() << "\n";
-		//cbica::Logging(loggerFile, "Error caught during testing: " + std::string(e.GetDescription()));
-		exit(EXIT_FAILURE);
-	}
-return reader->GetOutput();
-}
 template<class ImageType>
 VectorDouble PopulationAtlases::GetSpatialLocationFeaturesForFixedNumberOfRegions(const typename ImageType::Pointer &labelImagePointer, 
   const typename ImageType::Pointer &jacobtemplateImagePointer, 
