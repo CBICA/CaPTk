c1cf8c2f204653e8922076bf656a4a43f1ade572
PhucNgo1711 <phucnmrot@gmail.com>
2020-05-14 18:38:53 -0400

    fix error prev push

--- src/view/gui/fMainWindow.cpp
+++ src/view/gui/fMainWindow.cpp
@@ -1185,7 +1185,7 @@ void fMainWindow::unzipArchive(QString fullPath, QString extractPath, QString ap
 
     bool extracted = zr.extractAll(extractPath);
 
-    if (getApplicationDownloadPath("libra").emtpy()) {
+    if (getApplicationDownloadPath("libra").empty()) {
       ShowErrorMessage("Installation failed. Please re-run installtion.");
       stlapps->RetreiveAppSetting(appName);
       stlapps->Debug("Extraction failed");
