223e85a6ff94906b4def65dbe4b73ac826497bed
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2020-06-03 13:53:47 -0400

    singular function to handle bias correction more effectively

--- src/applications/Preprocessing/src/depends/BiasCorrection.hpp
+++ src/applications/Preprocessing/src/depends/BiasCorrection.hpp
@@ -92,6 +92,114 @@ public:
     int otsuBins = default_otsuBins
   );
 
+private:
+
+  //! provide a single template function to handle the bias correction
+  template< class TImageType, class TMaskImageType,
+    class TBiasCorrectorType >
+    typename TImageType::Pointer
+    algorithmRunner(
+      typename TImageType::Pointer inputImage,
+      typename TMaskImageType::Pointer inputMask,
+      int splineOrder,
+      int maxIterations,
+      int fittingLevels,
+      float filterNoise,
+      float fwhm,
+      int otsuBins,
+      std::string correctionType
+    )
+  {
+    // Downsample image for fast bias correction as recommended by ANTs developers
+    typedef itk::ShrinkImageFilter<TImageType, TImageType> ShrinkerType;
+    typename ShrinkerType::Pointer shrinker = ShrinkerType::New();
+    shrinker->SetInput(inputImage);
+    shrinker->SetShrinkFactors(4); // 4 is an arbitrary, but good, value
+    shrinker->Update();
+    auto inputShrunkImage = shrinker->GetOutput();
+
+    // Downsample the mask to match the downsampled image
+    typedef itk::ShrinkImageFilter<TMaskImageType, TMaskImageType> MaskShrinkerType;
+    typename MaskShrinkerType::Pointer maskShrinker = MaskShrinkerType::New();
+    maskShrinker->SetInput(inputMask);
+    maskShrinker->SetShrinkFactors(4);
+    maskShrinker->Update();
+    auto inputShrunkMask = maskShrinker->GetOutput();
+
+    auto biasCorrector = typename TBiasCorrectorType::New();
+    biasCorrector->SetInput(inputShrunkImage);
+    biasCorrector->SetMaskImage(inputShrunkMask);
+    biasCorrector->SetSplineOrder(splineOrder);
+    //biasCorrector->SetWeinerFilterNoise(filterNoise);
+    biasCorrector->SetBiasFieldFullWidthAtHalfMaximum(fwhm);
+    //biasCorrector->SetMaximumNumberOfIterations(maxIterations);
+    biasCorrector->SetConvergenceThreshold(0.0000001);
+    biasCorrector->SetNumberOfFittingLevels(fittingLevels); // increasing this exponentially increases execution time
+    try
+    {
+      biasCorrector->Update();
+    }
+    catch (std::exception &e)
+    {
+      logger.Write("Error caught while running " + correctionType + " bias correction: " + std::string(e.what()));
+    }
+
+
+    // Map the B-spline lattice from the N3 filter back into the original input image's space
+    typedef itk::BSplineControlPointImageFilter<typename TBiasCorrectorType::BiasFieldControlPointLatticeType,
+      typename TBiasCorrectorType::ScalarImageType> BSplinerType;
+    typename BSplinerType::Pointer bspliner = BSplinerType::New();
+    bspliner->SetInput(biasCorrector->GetLogBiasFieldControlPointLattice());
+    bspliner->SetSplineOrder(biasCorrector->GetSplineOrder());
+    bspliner->SetSize(inputImage->GetLargestPossibleRegion().GetSize());
+    bspliner->SetOrigin(inputImage->GetOrigin());
+    bspliner->SetDirection(inputImage->GetDirection());
+    bspliner->SetSpacing(inputImage->GetSpacing());
+    bspliner->Update();
+
+    auto logField = TImageType::New();
+    // logField is the log of the bias field
+    logField->SetOrigin(bspliner->GetOutput()->GetOrigin());
+    logField->SetSpacing(bspliner->GetOutput()->GetSpacing());
+    logField->SetRegions(bspliner->GetOutput()->GetLargestPossibleRegion().GetSize());
+    logField->SetDirection(bspliner->GetOutput()->GetDirection());
+    logField->Allocate();
+
+    // Copy the bspliner output into logField
+    typedef itk::N3MRIBiasFieldCorrectionImageFilter<TImageType, TMaskImageType, TImageType> TBiasCorrectorType;
+    itk::ImageRegionIterator<typename TBiasCorrectorType::ScalarImageType> ItB(bspliner->GetOutput(), bspliner->GetOutput()->GetLargestPossibleRegion());
+    itk::ImageRegionIterator<TImageType> ItF(logField, logField->GetLargestPossibleRegion());
+    for (ItB.GoToBegin(), ItF.GoToBegin(); !ItB.IsAtEnd(); ++ItB, ++ItF)
+    {
+      ItF.Set(ItB.Get()[0]);
+    }
+
+    // reconstruct original bias field from its log
+    typedef itk::ExpImageFilter<TImageType, TImageType> ExpFilterType;
+    typename ExpFilterType::Pointer expFilter = ExpFilterType::New();
+    expFilter->SetInput(logField);
+    expFilter->Update();
+
+    // Divide the input image by the reconstructed bias field (finally correcting the bias)
+    typedef itk::DivideImageFilter<TImageType, TImageType, TImageType> DividerType;
+    typename DividerType::Pointer divider = DividerType::New();
+    divider->SetInput1(inputImage);
+    divider->SetInput2(expFilter->GetOutput());
+    divider->Update();
+
+    auto outputImage = cbica::CreateImage< TImageType >(inputImage); // initialize outputImage
+
+    itk::ImageRegionIterator<TImageType> outputIterator(outputImage, outputImage->GetLargestPossibleRegion());
+    itk::ImageRegionIterator<TImageType> dividerIterator(divider->GetOutput(), biasCorrector->GetOutput()->GetLargestPossibleRegion());
+
+    // output the newly bias-corrected image
+    for (outputIterator.GoToBegin(), dividerIterator.GoToBegin(); !dividerIterator.IsAtEnd(); ++dividerIterator, ++outputIterator)
+    {
+      outputIterator.Set(dividerIterator.Get());
+    }
+
+    return outputImage;
+  }
 };
 
 // Begin templated function definitions
@@ -121,20 +229,6 @@ typename TImageType::Pointer BiasCorrection::Run(std::string correctionType, // 
 
   }
 
-  // Ensure the future output has the same properties as the input
-  const typename TImageType::SpacingType spacing = inputImage->GetSpacing();
-  const typename TImageType::PointType origin = inputImage->GetOrigin();
-  const typename TImageType::DirectionType direction = inputImage->GetDirection();
-  const typename TImageType::RegionType region = inputImage->GetLargestPossibleRegion();
-  typename TImageType::Pointer outputImage = TImageType::New();
-
-  outputImage->SetSpacing(spacing);
-  outputImage->SetLargestPossibleRegion(region);
-  outputImage->SetBufferedRegion(region);
-  outputImage->Allocate();
-  outputImage->SetOrigin(origin);
-  outputImage->SetDirection(direction);
-
 
   // Generate a mask using Otsu's thresholding if one isn't provided by the user
   if (!maskImage) // pointer to itk image is not initialized
@@ -149,131 +243,43 @@ typename TImageType::Pointer BiasCorrection::Run(std::string correctionType, // 
     maskImage = otsu->GetOutput();
   }
 
+  auto outputImage = cbica::CreateImage< TImageType >(inputImage);
 
   typename TImageType::Pointer logField = TImageType::New();
   if (correctionType == "n3" || correctionType == "N3")
   {
-    // Downsample image for fast bias correction as recommended by ANTs developers
-    typedef itk::ShrinkImageFilter<TImageType, TImageType> ShrinkerType;
-    typename ShrinkerType::Pointer shrinker = ShrinkerType::New();
-    shrinker->SetInput(inputImage);
-    shrinker->SetShrinkFactors(4); // 4 is an arbitrary, but good, value
-    shrinker->Update();
-
-    // Downsample the mask to match the downsampled image
-    typedef itk::ShrinkImageFilter<TMaskImageType, TMaskImageType> MaskShrinkerType;
-    typename MaskShrinkerType::Pointer maskshrinker = MaskShrinkerType::New();
-    maskshrinker->SetInput(maskImage);
-    maskshrinker->SetShrinkFactors(4);
-    maskshrinker->Update();
+    using TBiasCorrectorType = itk::N3MRIBiasFieldCorrectionImageFilter< TImageType, TMaskImageType, TImageType >;
 
     // Run the correction on the shurnken image and mask
-    typedef itk::N3MRIBiasFieldCorrectionImageFilter<TImageType, TMaskImageType, TImageType> CorrectorType;
-    typename CorrectorType::Pointer corrector = CorrectorType::New();
-    corrector->SetInput(shrinker->GetOutput());
-    corrector->SetMaskImage(maskshrinker->GetOutput());
-    corrector->SetSplineOrder(splineOrder);
-    corrector->SetWeinerFilterNoise(filterNoise);
-    corrector->SetBiasFieldFullWidthAtHalfMaximum(fwhm);
-    corrector->SetMaximumNumberOfIterations(maxIterations);
-    corrector->SetConvergenceThreshold(0.0000001);
-    corrector->SetNumberOfFittingLevels(fittingLevels); // increasing this exponentially increases execution time
-    try
-    {
-      corrector->Update();
-    }
-    catch (std::exception &e)
-    {
-      logger.Write("Error caught while running " + correctionType + " bias correction: " + std::string(e.what()));
-    }
-
-    // Map the B-spline lattice from the N3 filter back into the original input image's space
-    typedef itk::BSplineControlPointImageFilter<typename CorrectorType::BiasFieldControlPointLatticeType, typename CorrectorType::ScalarImageType> BSplinerType;
-    typename BSplinerType::Pointer bspliner = BSplinerType::New();
-    bspliner->SetInput(corrector->GetLogBiasFieldControlPointLattice());
-    bspliner->SetSplineOrder(corrector->GetSplineOrder());
-    bspliner->SetSize(inputImage->GetLargestPossibleRegion().GetSize());
-    bspliner->SetOrigin(inputImage->GetOrigin());
-    bspliner->SetDirection(inputImage->GetDirection());
-    bspliner->SetSpacing(inputImage->GetSpacing());
-    bspliner->Update();
-
-    // logField is the log of the bias field
-    logField->SetOrigin(bspliner->GetOutput()->GetOrigin());
-    logField->SetSpacing(bspliner->GetOutput()->GetSpacing());
-    logField->SetRegions(bspliner->GetOutput()->GetLargestPossibleRegion().GetSize());
-    logField->SetDirection(bspliner->GetOutput()->GetDirection());
-    logField->Allocate();
-
-    // Copy the bspliner output into logField
-    typedef itk::N3MRIBiasFieldCorrectionImageFilter<TImageType, TMaskImageType, TImageType> CorrectorType;
-    itk::ImageRegionIterator<typename CorrectorType::ScalarImageType> ItB(bspliner->GetOutput(), bspliner->GetOutput()->GetLargestPossibleRegion());
-    itk::ImageRegionIterator<TImageType> ItF(logField, logField->GetLargestPossibleRegion());
-    for (ItB.GoToBegin(), ItF.GoToBegin(); !ItB.IsAtEnd(); ++ItB, ++ItF)
-    {
-      ItF.Set(ItB.Get()[0]);
-    }
-
-    // reconstruct original bias field from its log
-    typedef itk::ExpImageFilter<TImageType, TImageType> ExpFilterType;
-    typename ExpFilterType::Pointer expFilter = ExpFilterType::New();
-    expFilter->SetInput(logField);
-    expFilter->Update();
-
-    // Divide the input image by the reconstructed bias field (finally correcting the bias)
-    typedef itk::DivideImageFilter<TImageType, TImageType, TImageType> DividerType;
-    typename DividerType::Pointer divider = DividerType::New();
-    divider->SetInput1(inputImage);
-    divider->SetInput2(expFilter->GetOutput());
-    divider->Update();
-
-
-    itk::ImageRegionIterator<TImageType> outputIterator(outputImage, outputImage->GetLargestPossibleRegion());
-    itk::ImageRegionIterator<TImageType> dividerIterator(divider->GetOutput(), corrector->GetOutput()->GetLargestPossibleRegion());
-
-    // output the newly bias-corrected image
-    for (outputIterator.GoToBegin(), dividerIterator.GoToBegin(); !dividerIterator.IsAtEnd(); ++dividerIterator, ++outputIterator)
-    {
-      outputIterator.Set(dividerIterator.Get());
-    }
-
+    outputImage = algorithmRunner<
+    TImageType, TMaskImageType, TBiasCorrectorType >(
+      inputImage, maskImage,
+      splineOrder,
+      maxIterations,
+      fittingLevels,
+      filterNoise,
+      fwhm,
+      otsuBins,
+      correctionType
+      );
+    
   } // End of N3
-
   else if (correctionType == "n4" || correctionType == "N4")
   {
-    // N4 currently doesn't use the downsamplng method used in N3.
-    // TODO: Apply the standard downsample-correct-reconstruct methodology to both N3 and N4.
-    // When this was last attempted, a bug arose where the BSplineControlPointImageFilter couldn't
-    // accept a LogBiasFieldControlPointLattice from the N4 correction filter.  
-    typedef itk::N4BiasFieldCorrectionImageFilter<TImageType, TMaskImageType, TImageType> CorrectorType;
-    typename CorrectorType::Pointer corrector = CorrectorType::New();
-    corrector->SetInput(inputImage);
-    corrector->SetMaskImage(maskImage);
-    corrector->SetSplineOrder(splineOrder);
-    corrector->SetWienerFilterNoise(filterNoise);
-    corrector->SetBiasFieldFullWidthAtHalfMaximum(fwhm);
-    // maxIterations doesn't work currently for N4 -- need to pass an array of maxIterations integers (1 per fitting level)
-    //corrector->SetMaximumNumberOfIterations(maxIterations);
-    corrector->SetConvergenceThreshold(0.0000001);
-    // fittingLevels are also disabled (see above comment) for the time being 
-    //corrector->SetNumberOfFittingLevels(fittingLevels);
-    try
-    {
-      corrector->Update();
-    }
-    catch (std::exception &e)
-    {
-      logger.Write("Error caught while running " + correctionType + " bias correction: " + std::string(e.what()));
-    }
+    using TBiasCorrectorType = itk::N4BiasFieldCorrectionImageFilter< TImageType, TMaskImageType, TImageType >;
 
-    itk::ImageRegionIterator<TImageType> outputIterator(outputImage, outputImage->GetLargestPossibleRegion());
-    itk::ImageRegionIterator<TImageType> correctedIterator(corrector->GetOutput(), corrector->GetOutput()->GetLargestPossibleRegion());
-
-    // output the newly bias-corrected image
-    for (outputIterator.GoToBegin(), correctedIterator.GoToBegin(); !correctedIterator.IsAtEnd(); ++correctedIterator, ++outputIterator)
-    {
-      outputIterator.Set(correctedIterator.Get());
-    }
+    // Run the correction on the shurnken image and mask
+    outputImage = algorithmRunner<
+      TImageType, TMaskImageType, TBiasCorrectorType >(
+        inputImage, maskImage,
+        splineOrder,
+        maxIterations,
+        fittingLevels,
+        filterNoise,
+        fwhm,
+        otsuBins,
+        correctionType
+        );
 
   } // End of N4
 
