a063ead7e5bf26398fa87cb9608c5b15fc41268f
PhucNgo1711 <phucnmrot@gmail.com>
2020-06-28 00:13:40 -0400

    include everything

--- src/applications/BreastTexturePipeline/src/BreastTexturePipeline.cxx
+++ src/applications/BreastTexturePipeline/src/BreastTexturePipeline.cxx
@@ -11,9 +11,9 @@
 
 #include "CaPTkDefines.h"
 #include "ApplicationPreferences.h"
-// #include "ApplicationDownloadManager.h"
+#include "ApplicationDownloadManager.h"
 // #include "ThreadedExtraction.h"
-#include "QZipReader.h"
+// #include "QZipReader.h"
 #include "yaml-cpp/node/node.h"
 
 
