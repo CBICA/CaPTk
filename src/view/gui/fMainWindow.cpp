998104486ebbf58c10717eed2556fd0ef9fe3e49
PhucNgo1711 <phucnmrot@gmail.com>
2020-06-28 12:47:06 -0400

    no ref in main

--- src/view/gui/fMainWindow.cpp
+++ src/view/gui/fMainWindow.cpp
@@ -72,7 +72,7 @@
 
 #include <QFile>
 
-#include "ApplicationDownloadManager.h"
+// #include "ApplicationDownloadManager.h"
 
 // this function calls an external application from CaPTk in the most generic way while waiting for output
 int fMainWindow::startExternalProcess(const QString &application, const QStringList &arguments)
@@ -5929,8 +5929,8 @@ void fMainWindow::openDicomImages(QString dir)
 
 void fMainWindow::ApplicationLIBRABatch()
 {
-  ApplicationDownloadManager* appDownloadMngr = new ApplicationDownloadManager();
-  std::string scriptToCall = appDownloadMngr->getApplication("libra");
+  // ApplicationDownloadManager* appDownloadMngr = new ApplicationDownloadManager();
+  // std::string scriptToCall = appDownloadMngr->getApplication("libra");
 
   connect(appDownloadMngr, SIGNAL(updateProgressSignal(int, std::string, int)), this, SLOT(updateProgress(int, std::string, int)));
   // connect(appDownloadMngr, SIGNAL(updateProgressExtract(int, std::string, int)), this, SLOT(updateProgress(int, std::string, int)));
@@ -6038,8 +6038,8 @@ void fMainWindow::ApplicationBreastSegmentation()
 
   updateProgress(15, "Initializing and running LIBRA compiled by MCC");
 
-  ApplicationDownloadManager* appDownloadMngr = new ApplicationDownloadManager();
-  std::string scriptToCall = appDownloadMngr->getApplication("libra");
+  // ApplicationDownloadManager* appDownloadMngr = new ApplicationDownloadManager();
+  // std::string scriptToCall = appDownloadMngr->getApplication("libra");
 
   connect(appDownloadMngr, SIGNAL(updateProgressSignal(int, std::string, int)), this, SLOT(updateProgress(int, std::string, int)));
   // connect(appDownloadMngr, SIGNAL(updateProgressExtract(int, std::string, int)), this, SLOT(updateProgress(int, std::string, int)));
@@ -6101,8 +6101,8 @@ void fMainWindow::ApplicationLIBRASingle()
 
   updateProgress(15, "Initializing and running LIBRA compiled by MCC");
 
-  ApplicationDownloadManager* appDownloadMngr = new ApplicationDownloadManager();
-  std::string scriptToCall = appDownloadMngr->getApplication("libra");
+  // ApplicationDownloadManager* appDownloadMngr = new ApplicationDownloadManager();
+  // std::string scriptToCall = appDownloadMngr->getApplication("libra");
 
   connect(appDownloadMngr, SIGNAL(updateProgressSignal(int, std::string, int)), this, SLOT(updateProgress(int, std::string, int)));
   // connect(appDownloadMngr, SIGNAL(updateProgressExtract(int, std::string, int)), this, SLOT(updateProgress(int, std::string, int)));
