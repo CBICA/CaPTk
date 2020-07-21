4640b521a6d1efa167015fc230605ebf389915b4
sarthakpati <sarthak.pati@hotmail.com>
2020-07-20 21:43:58 -0400

    html updates for documentation

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -63,7 +63,7 @@ var NAVTREE =
       [ "Is there a minimum recommended screen resolution for running CaPTk?", "FAQ.html#gs_FAQ_6", null ],
       [ "Where can I find the CaPTk Docker Image?", "FAQ.html#gs_FAQ_7", null ],
       [ "How do I run the CaPTk Docker Image to show the interface?", "FAQ.html#gs_FAQ_8", null ],
-      [ "What if I am having an issue not listed here?", "FAQ.html#gs_FAQ_9", null ]
+      [ "Is there something wrong with Feature Extraction?", "FAQ.html#gs_FAQ_9", null ]
     ] ],
     [ "How To Guides", "How_To_Guides.html", "How_To_Guides" ],
     [ "Scientific Findings using CaPTk", "Science.html", [
