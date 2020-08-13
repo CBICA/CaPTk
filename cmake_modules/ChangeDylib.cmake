edb376c42d571310d3a73ea1b4a616a068cc19b8
alexandergetka <alexgetka@gmail.com>
2020-08-12 23:13:39 -0400

    more detailed check on utilities target
    if this fails something else is causing it to fail.

--- cmake_modules/ChangeDylib.cmake
+++ cmake_modules/ChangeDylib.cmake
@@ -1,10 +1,10 @@
 function(changeLibOmpDylib target application)
-  message(STATUS "adding post-build libomp install_name_tool change command for target ${target} and application ${application}") 
   changeDylib(${target} ${application} libomp.dylib /usr/local/opt/libomp/lib)
   
 endfunction()
 
 macro(changeDylib target application libname libpath)
+  message(STATUS "[DEBUG] adding post-build libomp install_name_tool change command for target ${target} application ${application} libname ${libname} libpath ${libpath} on executable ${CMAKE_BINARY_DIR}/${application}") 
   add_custom_command(TARGET ${target} POST_BUILD
   COMMAND 
   install_name_tool -change \"${libpath}/${libname}\" \"@executable_path/../../Frameworks/${libname}\" \"${CMAKE_BINARY_DIR}/${application}\"
