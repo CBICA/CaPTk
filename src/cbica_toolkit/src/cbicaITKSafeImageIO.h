cbd66f618c722355649f65c3ad8c35b2af669bd3
sarthakpati <sarthak.pati@hotmail.com>
2020-08-26 13:01:41 -0400

    minor text change

--- src/cbica_toolkit/src/cbicaITKSafeImageIO.h
+++ src/cbica_toolkit/src/cbicaITKSafeImageIO.h
@@ -826,7 +826,7 @@ namespace cbica
       // get the default series description and add some information to make it unique
       std::string seriesDescription;
       itk::ExposeMetaData< std::string >(*dictArray->at(0), "0008|103e", seriesDescription);
-      seriesDescription += "_Processed-by-CaPTk";
+      seriesDescription += "_Processed-CaPTk";
 
       for (size_t i = 0; i < dictArray->size(); i++)
       {
