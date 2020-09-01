d606cfab422db674701cc6e85f573e13ead218a4
Ashish Singh <ashish.singh@uphs.upenn.edu>
2020-09-01 16:10:27 -0400

    adding download signal for survival, #1226

--- src/view/gui/fSurvivalDialog.h
+++ src/view/gui/fSurvivalDialog.h
@@ -71,6 +71,9 @@ signals:
   void TrainNewSurvivalPredictionModel(const std::string inputdirectory, const std::string outputdirectory);
   void SurvivalPredictionOnExistingModel(const std::string modeldirectory, const std::string inputdirectory, const std::string outputdirectory);
   void SubjectBasedSurvivalEstimate(const std::string output, const std::string model, double age);
+
+  //signal to download from this url
+  void DownloadUrl(QUrl &);
 };
 
 
