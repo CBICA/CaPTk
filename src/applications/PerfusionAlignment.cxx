0c94dad5c0f98499f77e2795815597cc01956274
sarthakpati <sarthak.pati@hotmail.com>
2020-10-27 12:20:19 -0400

    updated parsing

--- src/applications/PerfusionAlignment.cxx
+++ src/applications/PerfusionAlignment.cxx
@@ -33,9 +33,9 @@ next patch release, expected in Q4 2020.\n";
   //parser.addRequiredParameter("a", "timePointsAfterDrop", cbica::Parameter::STRING, "0-100", "The number of time-points after the drop.");
   //parser.addRequiredParameter("t", "timeDomainResolution", cbica::Parameter::FLOAT, "0.01-100", "The time-interval (spacing) between two consecutive volumes in time-domain (in seconds).");
 
-  parser.addOptionalParameter("d", "dropScaling", cbica::Parameter::BOOLEAN, "0-1", "Whether to scale the value of the drop for the curve? 1=yes, 0=no; defaults to '0'");
-  parser.addOptionalParameter("s", "stdDev", cbica::Parameter::FLOAT, "0-100", "The standard deviation threshold of time series signal above which the", "location is considered to part of brain", "Defaults to " + std::to_string(stdDev));
-  parser.addOptionalParameter("bl", "baseLine", cbica::Parameter::FLOAT, "0-1000", "The value of the baseline to which the output gets scaled", "Only used if '-d' is '1'", "Defaults to " + std::to_string(baseline));
+  //parser.addOptionalParameter("d", "dropScaling", cbica::Parameter::BOOLEAN, "0-1", "Whether to scale the value of the drop for the curve? 1=yes, 0=no; defaults to '0'");
+  //parser.addOptionalParameter("s", "stdDev", cbica::Parameter::FLOAT, "0-100", "The standard deviation threshold of time series signal above which the", "location is considered to part of brain", "Defaults to " + std::to_string(stdDev));
+  //parser.addOptionalParameter("bl", "baseLine", cbica::Parameter::FLOAT, "0-1000", "The value of the baseline to which the output gets scaled", "Only used if '-d' is '1'", "Defaults to " + std::to_string(baseline));
   parser.addOptionalParameter("L", "Logger", cbica::Parameter::STRING, "log file which user has write access to", "Full path to log file to store console outputs", "By default, only console output is generated");
   //parser.exampleUsage("PerfusionAlignment -i AAAC_PreOp_perf_pp.nii.gz -d AAAC_PreOp_perf_pp.dcm -o <output dir>");
   parser.addExampleUsage("-i AAAC_PreOp_perf_pp.nii.gz -c AAAC_PreOp_t1ce_pp.nii.gz -b 15 -a 17 -t 2 -o <output dir>", "Aligns the perfusion signal of the input image based on the time points");
@@ -45,12 +45,13 @@ next patch release, expected in Q4 2020.\n";
   cbica::Logging logger;
   std::string loggerFile;
   bool loggerRequested = false;
-  std::string inputFileName, inputDicomName, outputDirectoryName, inputt1ceName;
+  std::string inputFileName, inputDicomName, outputDirectoryName;
 
   parser.getParameterValue("i", inputFileName);
   parser.getParameterValue("t1", time_inputPerfTime);
   parser.getParameterValue("t3", time_beforeDrop);
   parser.getParameterValue("t4", time_afterDrop);
+  parser.getParameterValue("o", outputDirectoryName);
 
   if (parser.isPresent("t2"))
   {
@@ -61,42 +62,19 @@ next patch release, expected in Q4 2020.\n";
       time_outputPerfTime = 1;
     }
   }
-
+  if (parser.isPresent("s1"))
+  {
+    parser.getParameterValue("s1", scale_maxIntensityBeforeDrop);
+  }
+  if (parser.isPresent("s2"))
+  {
+    parser.getParameterValue("s2", scale_intensityDropInMeanCurve);
+  }
   if (parser.compareParameter("L", tempPosition))
   {
     loggerFile = argv[tempPosition + 1];
     loggerRequested = true;
     logger.UseNewFile(loggerFile);
-  }
-  if (parser.compareParameter("i", tempPosition))
-  {
-    inputFileName = argv[tempPosition + 1];
-  }
-  if (parser.compareParameter("o", tempPosition))
-  {
-    outputDirectoryName = argv[tempPosition + 1];
-  }
-
-  if (parser.compareParameter("b", tempPosition))
-    pointsbeforedrop = atoi(argv[tempPosition + 1]);
-
-  if (parser.compareParameter("t", tempPosition))
-    timeresolution = atof(argv[tempPosition + 1]);
-
-  if (parser.compareParameter("a", tempPosition))
-    pointsafterdrop = atoi(argv[tempPosition + 1]);
-
-  if (parser.isPresent("d"))
-  {
-    parser.getParameterValue("d", dropscaling);
-  }
-  if (parser.isPresent("s"))
-  {
-    parser.getParameterValue("s", stdDev);
-  }
-  if (parser.isPresent("bl"))
-  {
-    parser.getParameterValue("bl", baseline);
   }
 
   if (!cbica::isFile(inputFileName))
