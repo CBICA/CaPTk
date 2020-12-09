b4b976e993871fdc5c5a66b39a4e76afd490d61a
Ashish Singh <ashish.singh@uphs.upenn.edu>
2020-12-09 14:30:34 -0500

    adding comments

--- src/applications/PerfusionPCA.cxx
+++ src/applications/PerfusionPCA.cxx
@@ -49,19 +49,17 @@ std::vector<std::map<CAPTK::ImageModalityType, std::string>> LoadQualifiedSubjec
 
 int main(int argc, char **argv)
 {
-  //std::cout << "This functionality has been removed from this CaPTk release, and we are actively working on an optimized robust implementation that should enable generalization in multi-institutional data. We expect this to be released in our next patch release, in Q4 2020.\n";
-  //return EXIT_FAILURE;
   cbica::CmdParser parser = cbica::CmdParser(argc, argv, "PerfusionPCA");
   parser.addRequiredParameter("i", "input", cbica::Parameter::STRING, "", "The input directory.");
-  parser.addRequiredParameter("t", "type", cbica::Parameter::INTEGER, "", "The option of preparing a new model (=0), and for testing on an existing model (=1)");
+  parser.addRequiredParameter("t", "type", cbica::Parameter::INTEGER, "", "The option of extracting a new PCA parameters (=0), and for applying existing PCA parameters (=1)");
   parser.addOptionalParameter("n", "number of PCAs", cbica::Parameter::FLOAT, "", "The number of principal components.");
-  parser.addOptionalParameter("m", "model", cbica::Parameter::STRING, "", "The directory having PCA models");
+  parser.addOptionalParameter("m", "model", cbica::Parameter::STRING, "", "The directory having PCA parameters");
   parser.addOptionalParameter("vt", "variance threshold", cbica::Parameter::FLOAT, "", "The variance threshold");
   parser.addOptionalParameter("dif", "dump intermediate files", cbica::Parameter::BOOLEAN, "", "Write intermediate file containing perfusion data for whole population.");
   parser.addRequiredParameter("o", "output", cbica::Parameter::STRING, "", "The output directory.");
   parser.addOptionalParameter("L", "Logger", cbica::Parameter::STRING, "log file which user has write access to", "Full path to log file to store console outputs", "By default, only console output is generated");
-  parser.addExampleUsage("-t 0 -i C:/properly/formatted/inputDir -o C:/outputDir -n 5", "Trains a new model based on the samples in inputDir");
-  parser.addExampleUsage("-t 1 -i C:/input -m C:/model -o C:/output -m C:/modelDir -n 5", "Tests an existing model for inputs in 'C:/input' based on 'C:/modelDir' ");
+  parser.addExampleUsage("-t 0 -i C:/properly/formatted/inputDir -o C:/outputDir -n 5", "Extracts new PCA parameters based on the samples in inputDir");
+  parser.addExampleUsage("-t 1 -i C:/input -o C:/output -m C:/modelDir -n 5", "Tests existing PCA parameters for inputs in 'C:/input' based on 'C:/modelDir' ");
   parser.addApplicationDescription("Perfusion PCA Training and Prediction application");
 
   // parameters to get from the command line
@@ -79,6 +77,7 @@ int main(int argc, char **argv)
   bool varianceThresholdDefined = false;
   bool dif = false;
 
+  //get params supplied by user
   parser.getParameterValue("i", inputFileName);
   parser.getParameterValue("o", outputDirectoryName);
   parser.getParameterValue("t", applicationType);
@@ -89,8 +88,6 @@ int main(int argc, char **argv)
   }
   else
 	  dif = false;
-
-  std::cout << " dif: " << dif << std::endl;
 
   if (parser.isPresent("n"))
   {
@@ -112,7 +109,6 @@ int main(int argc, char **argv)
 	  return EXIT_FAILURE;
   }
 
-
   if (parser.isPresent("m"))
   {
     parser.getParameterValue("m", modelDirectoryName);
@@ -124,14 +120,14 @@ int main(int argc, char **argv)
     logger.UseNewFile(loggerFile);
   }
 
-  std::cout << "Input File:" << inputFileName << std::endl;
-  std::cout << "Output Directory:" << outputDirectoryName << std::endl;
-
+  //check if input dir exists
   if (!cbica::isDir(inputFileName))
   {
     std::cout << "The input directory does not exist:" << inputFileName << std::endl;
     return EXIT_FAILURE;
   }
+
+  //check if output dir exists
   if (!cbica::directoryExists(outputDirectoryName))
   {
     if (!cbica::createDirectory(outputDirectoryName))
@@ -139,20 +135,21 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  //std::vector<std::map<CAPTK::ImageModalityType, std::string>> QualifiedSubjects = LoadQualifiedSubjectsFromGivenDirectoryForPCA(inputFileName);
-  PerfusionPCA object_pca;
+  PerfusionPCA object_pca; //pca algorithm object
+
+  //sort and arrange input data
   object_pca.LoadQualifiedSubjectsFromGivenDirectoryForPCA(inputFileName);
 
-  //if (QualifiedSubjects.size() == 0)
+  //check if input has valid subjects
   if(!object_pca.HasValidSubjects())
   {
     std::cout << "There is no subject with the required input in the given directory." << std::endl;
     return EXIT_FAILURE;
   }
-  //std::cout << "Number of subjects with the required input: " << QualifiedSubjects.size() << std::endl;
 
+  //loads the input dataset
   std::string inValidSubject;
-  if (object_pca.LoadData(/*QualifiedSubjects,*/ inValidSubject) == PerfusionPCA::DifferentTimePoints)
+  if (object_pca.LoadData(inValidSubject) == PerfusionPCA::DifferentTimePoints)
   {
 	  std::string msg = "Could not load data. Please check that all input data has the same number of time points. Look at file: " + inValidSubject;
 	  std::cout << msg << std::endl;
@@ -161,20 +158,23 @@ int main(int argc, char **argv)
 
   if (applicationType == CAPTK::MachineLearningApplicationSubtype::TESTING)
   {
-	  //TBD: during testing read the number of PCs from file 
-    std::cout << "Model directory name:" << modelDirectoryName << std::endl;
+	  //check if PCA parameter dir exists
     if (!cbica::directoryExists(modelDirectoryName))
     {
       std::cout << "The model directory does not exist:" << modelDirectoryName << std::endl;
       return EXIT_FAILURE;
     }
+
+	//check if PCA parameter dir contains the required files
     if (!cbica::fileExists(modelDirectoryName +"/PCA_PERF.csv") || 
 		!cbica::fileExists(modelDirectoryName + "/Mean_PERF.csv") ||
 		!cbica::fileExists(modelDirectoryName + "/PCCumulativeVariance.csv"))
     {
-      std::cout << "The required model files PCA_PERF.csv, Mean_PERF.csv and PCCumulativeVariance.csv do not exist in the model directory:" <<  modelDirectoryName << std::endl;
+      std::cout << "The required files PCA_PERF.csv, Mean_PERF.csv and PCCumulativeVariance.csv do not exist in the PCA parameter directory:" <<  modelDirectoryName << std::endl;
       return EXIT_FAILURE;
     }
+
+	// check if version is compatible
     if (cbica::isFile(modelDirectoryName + "/VERSION.yaml"))
     {
         if (!cbica::IsCompatible(modelDirectoryName + "/VERSION.yaml"))
@@ -184,17 +184,20 @@ int main(int argc, char **argv)
         }
     }
 
+	//when both number of PCA imagea and variance threshold are not provided by the user
 	if (!nPCsDefined && !varianceThresholdDefined)
 	{
 		std::cout << "Please provide either the number of principal components or the variance threshold and re-run the application." << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	//set number of PCs on the object
+	//pass params to object
 	if (nPCsDefined)
 		object_pca.SetNumberOfPCs(inputPCs);
 	else if (varianceThresholdDefined)
 		object_pca.SetVarianceThreshold(varianceThreshold);
+
+	//call the algorithm
     PerfusionPCA::ErrorCode code = object_pca.ApplyExistingPCAModel(inputPCs, inputFileName, outputDirectoryName/*, QualifiedSubjects*/,modelDirectoryName);
 	if (code == PerfusionPCA::ErrorCode::DifferentTimePoints)
 	{
@@ -210,9 +213,9 @@ int main(int argc, char **argv)
   }
   else if (applicationType == CAPTK::MachineLearningApplicationSubtype::TRAINING)
   {
-		//if user doesnt provide n or vt, give a msg stating that we will provide only PCA parameters 
+		//if user doesn't provide n or vt, give a msg stating that we will provide only PCA parameters 
 		// and not the PCA images and that the run will take quite some time and request for confirmation
-		// press y to continue or n to exit and provide n or vt.
+		// press y to continue or n to exit(and provide n or vt).
 	  if (!nPCsDefined && !varianceThresholdDefined)
 	  {
 		  std::cout << "You did not provide the number of principal components or the variance threshold. \
@@ -231,7 +234,7 @@ Do you want to continue? Press 'y' to contine or 'n' (and press Enter) to exit a
 		  }
 	  }
 
-	  //spit a txt file : NumberofPCs having only a single number of PCs
+	  //pass params to object
 	  if (nPCsDefined)
 		  object_pca.SetNumberOfPCs(inputPCs);
 	  else if (varianceThresholdDefined)
@@ -241,7 +244,8 @@ Do you want to continue? Press 'y' to contine or 'n' (and press Enter) to exit a
 
 	  object_pca.RequestPerfusionDataWholePopulation(dif);//dump intermediate files or not
 
-	  if (object_pca.TrainNewPerfusionModel(inputPCs, inputFileName, outputDirectoryName/*, QualifiedSubjects*/))
+	  //calling the algorithm 
+	  if (object_pca.TrainNewPerfusionModel(inputPCs, inputFileName, outputDirectoryName))
 	  {
 		  std::cout << "principal components have been saved at the specified locations.\n";
 		  std::cout << "Finished successfully.\n";
